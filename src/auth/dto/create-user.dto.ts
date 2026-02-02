67c7a26569ddf406a9e95ad00e932d1068e15aca
Sotatek-LuongTran <luong.tran@sotatek.com>
2026-02-02 10:02:21 +0700

    feat: set up swagger and readme

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,14 +1,27 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
-export class CreateUserDto{
-    @IsEmail()
-    email: string;
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'example@email.com',
+    description: 'Email',
+  })
+  @IsEmail()
+  email: string;
 
-    @IsString()
-    @MinLength(6)
-    name: string;
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'Name',
+  })
+  @IsString()
+  @MinLength(6)
+  name: string;
 
-    @IsString()
-    @MinLength(6)
-    password: string;
+  @ApiProperty({
+    example: 'Supersecret123!',
+    description: 'Password',
+  })
+  @IsString()
+  @MinLength(6)
+  password: string;
 }
