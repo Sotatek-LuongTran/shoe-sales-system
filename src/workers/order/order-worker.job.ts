8ed8906310235179b2de757ad7d7c0b3f8882faa
Sotatek-LuongTran <luong.tran@sotatek.com>
2026-03-11 09:55:35 +0700

    ref: apply transaction and lock for order worker

--- src/workers/order/order-worker.job.ts
+++ src/workers/order/order-worker.job.ts
@@ -6,6 +6,7 @@ import { PaymentStatusEnum } from 'src/shared/enums/payment.enum';
 import { OrderRepository } from 'src/shared/modules/common-order/order.repository';
 import { PaymentRepository } from 'src/shared/modules/common-payment/payment.repository';
 import { ProductVariantRepository } from 'src/shared/modules/common-product-variant/product-variant.repository';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class OrderWorkerJob {
@@ -13,39 +14,47 @@ export class OrderWorkerJob {
     private readonly orderRepository: OrderRepository,
     private readonly productVariantRepository: ProductVariantRepository,
     private readonly paymentRepository: PaymentRepository,
+    private readonly dataSource: DataSource,
   ) {}
   @Cron(CronExpression.EVERY_30_SECONDS)
   async cancelExpiredOrders() {
     console.log('Start cancel expired orders . . .');
-    const pendingOrders = await this.orderRepository.findAllPendingOrders();
+    await this.dataSource.transaction(async (manager) => {
+      const pendingOrders = await manager
+        .withRepository(this.orderRepository)
+        .findAllPendingOrders();
 
-    for (const order of pendingOrders) {
-      for (const item of order.items) {
-        const variant =
-          await this.productVariantRepository.findByProductAndValue(
-            item.productId,
-            item.variantValue,
-          );
+      for (const order of pendingOrders) {
+        for (const item of order.items) {
+          const variant = await manager
+            .withRepository(this.productVariantRepository)
+            .findByProductAndValue(item.productId, item.variantValue);
 
-        if (!variant) {
-          throw new NotFoundException({
-            errorCode: ErrorCodeEnum.PRODUCT_VARIANT_NOT_FOUND,
-            statusCode: 404,
-            message: 'Variant not found',
-          });
+          if (!variant) {
+            throw new NotFoundException({
+              errorCode: ErrorCodeEnum.PRODUCT_VARIANT_NOT_FOUND,
+              statusCode: 404,
+              message: 'Variant not found',
+            });
+          }
+
+          variant.reservedStock -= item.quantity;
+          await manager
+            .withRepository(this.productVariantRepository)
+            .save(variant);
         }
+        order.status = OrderStatusEnum.EXPIRED;
+        await manager.withRepository(this.orderRepository).save(order);
 
-        variant.reservedStock -= item.quantity;
-        await this.productVariantRepository.save(variant);
+        order.payment.paymentStatus = PaymentStatusEnum.CANCELLED;
+        await manager
+          .withRepository(this.paymentRepository)
+          .save(order.payment);
+        console.log(
+          `- Cancelled order: ${order.id} with payment: ${order.payment.id}.`,
+        );
       }
-      
-      order.status = OrderStatusEnum.EXPIRED;
-      await this.orderRepository.save(order);
-
-      order.payment.paymentStatus = PaymentStatusEnum.CANCELLED;
-      await this.paymentRepository.save(order.payment);
-      console.log(`- Cancelled order: ${order.id} with payment: ${order.payment.id}.`)
-    }
+    });
     console.log('Cancel expired orders completed.');
   }
 }
